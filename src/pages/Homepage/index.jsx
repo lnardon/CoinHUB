53b5733a1dd89050da65dd5b51c71b4d51c5bc6e
lnardon <lucaslnardon@gmail.com>
2020-10-25 04:14:15 -0300

    🚧 Add logo and basic homepage

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
-// import { Container } from './styles';
+import "./styles.css";
 
 function Homepage() {
-  return <div />;
+  return (
+    <div className="homepageContainer">
+      <div className="logoDiv">
+        <img
+          src={require("../../assets/logo.png")}
+          alt="Logo"
+          className="logo"
+        />
+      </div>
+      <input type="text" className="searchbar" placeholder="Search..." />
+    </div>
+  );
 }
 
 export default Homepage;
