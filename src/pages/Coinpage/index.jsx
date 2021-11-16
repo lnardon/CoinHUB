4157dd458f92a242b720f7291c74800f781303b2
Lucas Nardon <lucaslnardon@gmail.com>
2021-11-16 13:17:03 -0300

    ⬆️ Update dependencies

--- src/pages/Coinpage/index.jsx
+++ src/pages/Coinpage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { Line } from "react-chartjs-2";
 
